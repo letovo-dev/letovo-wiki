537c243f7b06f915ae3bd97cc3cd979c077ee2a2
Zkh-dot <kernzahar@gmail.com>
2025-01-22 15:25:57 +0300

    fix page server

--- page_server.cc
+++ page_server.cc
@@ -1,4 +1,79 @@
 #include "page_server.h"
+
+namespace page {
+
+    pqxx::result get_page_content(int post_id, std::shared_ptr<cp::ConnectionsManager> pool_ptr) {
+        auto con = std::move(pool_ptr->getConnection());
+        std::vector<int> params = {post_id};
+        pqxx::result result = con->execute_params("SELECT * FROM \"posts\" WHERE \"post_id\"=($1);", params);
+        pool_ptr->returnConnection(std::move(con));
+        return result;
+    }
+
+    pqxx::result get_page_author(std::string username, std::shared_ptr<cp::ConnectionsManager> pool_ptr) {
+        auto con = std::move(pool_ptr->getConnection());
+        std::vector<std::string> params = {username};
+        pqxx::result result = con->execute_params("SELECT \"username\", \"avatar_pic\" from \"user\" WHERE \"username\"=($1);", params);
+        pool_ptr->returnConnection(std::move(con));
+        return result;
+    }
+
+    int add_page_by_content(bool is_secret, bool is_published, int likes, std::string title, std::string author, std::string text, std::string post_path, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr) {
+        auto con = std::move(pool_ptr->getConnection());
+        std::vector<std::string> params = {post_path, std::to_string(is_secret), std::to_string(is_published), std::to_string(likes), title, author, text};
+        pqxx::result result = con->execute_params("INSERT INTO \"posts\" (\"post_path\", \"is_secret\", \"is_published\", \"likes\", \"title\", \"author\", \"text\") VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING \"post_id\";", params);
+        pool_ptr->returnConnection(std::move(con));
+        return result[0]["post_id"].as<int>();
+    }
+
+    int add_page_by_page(std::string post_path, std::string text, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr) {
+        auto con = std::move(pool_ptr->getConnection());
+        std::vector<std::string> params = {post_path};
+        pqxx::result result = con->execute_params("INSERT INTO \"posts\" (\"post_path\") VALUES ($1) RETURNING \"post_id\";", params);
+        pool_ptr->returnConnection(std::move(con));
+        assist::create_file(post_path, text, logger_ptr);
+        return result[0]["post_id"].as<int>();
+    }
+
+    void update_likes(int post_id, int likes, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr) {
+        auto con = std::move(pool_ptr->getConnection());
+        std::vector<std::string> params = {std::to_string(likes), std::to_string(post_id)};
+        con->execute_params("UPDATE \"posts\" SET \"likes\"=($1) WHERE \"post_id\"=($2);", params, true);
+        pool_ptr->returnConnection(std::move(con));
+    }
+
+    void delete_page(int post_id, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr) {
+        auto con = std::move(pool_ptr->getConnection());
+        std::vector<std::string> params = {std::to_string(post_id)};
+        con->execute_params("DELETE FROM \"posts\" WHERE \"post_id\"=($1);", params, true);
+        pool_ptr->returnConnection(std::move(con));
+    }
+
+    pqxx::result get_favourite_posts(std::string username, std::shared_ptr<cp::ConnectionsManager> pool_ptr) {
+        auto con = std::move(pool_ptr->getConnection());
+        std::vector<std::string> params = {username};
+        pqxx::result result = con->execute_params("SELECT username, post_id from \"favourite_posts\" WHERE \"username\"=($1);", params);
+        pool_ptr->returnConnection(std::move(con));
+        return result;
+    }
+
+    pqxx::result add_favourite_post(int post_id, std::string username, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr) {
+        auto con = std::move(pool_ptr->getConnection());
+        std::vector<std::string> params = {std::to_string(post_id), username};
+        pqxx::result result = con->execute_params("INSERT INTO \"favourite_posts\" (\"post_id\", \"username\") VALUES ($1, $2) ON CONFLICT (\"post_id\", \"username\") DO NOTHING;", params, true);
+        pool_ptr->returnConnection(std::move(con));
+        return result;
+    }
+
+    void delete_favourite_post(int post_id, std::string username, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr) {
+        auto con = std::move(pool_ptr->getConnection());
+        std::vector<std::string> params = {std::to_string(post_id), username};
+        con->execute_params("DELETE FROM \"favourite_posts\" WHERE \"post_id\"=($1) AND \"username\"=($2);", params, true);
+        pool_ptr->returnConnection(std::move(con));
+    }
+}
+
+
 namespace page::server {
     void get_page_content(std::unique_ptr<restinio::router::express_router_t<>>& router, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr) {
         router.get()->http_get(R"(/post/:id(\d+))", [pool_ptr, logger_ptr](auto req, auto params) {
@@ -11,14 +86,10 @@ namespace page::server {
                 return req->create_response(restinio::status_bad_request()).done();
             }
 
-            cp::query get_page("SELECT * FROM \"posts\" WHERE \"post_id\"=($1);");
-
-            auto tx = cp::tx(*pool_ptr, get_page);
-
-            pqxx::result result = get_page(postid);
-            if (result[0]["is_secret"].as<bool>() == true, result[0]["is_published"].as<bool>() == false) {
+            pqxx::result result = page::get_page_content(postid, pool_ptr);
+            if (result[0]["is_secret"].as<bool>() == true || result[0]["is_published"].as<bool>() == false) {
                 logger_ptr->info( [endpoint]{return fmt::format("page request from {} is secret", endpoint);});
-                return req->create_response(restinio::status_non_authoritative_information()).done();
+                return req->create_response(restinio::status_not_found()).done();
             }
 
             if(result.empty()) {
@@ -41,11 +112,7 @@ namespace page::server {
                 return req->create_response(restinio::status_bad_request()).done();
             }
 
-            cp::query get_page("SELECT \"username\", \"avatar_pic\" from \"user\" WHERE \"username\"=($1);");
-
-            auto tx = cp::tx(*pool_ptr, get_page);
-
-            pqxx::result result = get_page(username);
+            pqxx::result result = page::get_page_author(username, pool_ptr);
             
             if(result.empty()) {
                 return req->create_response(restinio::status_bad_gateway()).done();
@@ -77,18 +144,17 @@ namespace page::server {
                     logger_ptr->info( [endpoint]{return fmt::format("bad path from {}", endpoint);});
                     return req->create_response(restinio::status_forbidden()).done();
                 }
-                cp::query add_page("INSERT INTO \"posts\" (\"post_path\", \"is_secret\", \"is_published\", \"likes\", \"title\", \"author\", \"text\") VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING \"post_id\";");
-                auto tx = cp::tx(*pool_ptr, add_page);
-                pqxx::result res;
+                int post_id;
                 try {
-                    res = add_page(path, 
-                            std::any_cast<bool>(new_map["is_secret"]), 
-                            std::any_cast<bool>(new_map["is_published"]), 
-                            std::any_cast<int>(new_map["likes"]), 
-                            std::any_cast<std::string>(new_map["title"]), 
-                            std::any_cast<std::string>(new_map["author"]), 
-                            std::any_cast<std::string>(new_map["text"]));
-                            tx.commit();
+                    post_id = page::add_page_by_content(
+                        std::any_cast<bool>(new_map["is_secret"]),
+                        std::any_cast<bool>(new_map["is_published"]),
+                        std::any_cast<int>(new_map["likes"]),
+                        std::any_cast<std::string>(new_map["title"]),
+                        std::any_cast<std::string>(new_map["author"]),
+                        std::any_cast<std::string>(new_map["text"]),
+                        std::any_cast<std::string>(new_map["post_path"]),
+                        pool_ptr, logger_ptr);
                 } catch (pqxx::sql_error& e) {
                     logger_ptr->error( [endpoint]{return fmt::format("sql error from {}", endpoint);});
                     return req->create_response(restinio::status_non_authoritative_information()).done();
@@ -96,14 +162,13 @@ namespace page::server {
                     logger_ptr->error( [endpoint]{return fmt::format("bad request from {}", endpoint);});
                     return req->create_response(restinio::status_non_authoritative_information()).done();
                 }
-                int post_id = res[0]["post_id"].as<int>();
                 
                 logger_ptr->info( [post_id]{return fmt::format("page added with id {}", post_id);});
                 if (new_map["author"].has_value()) 
                     assist::create_mdx_from_template_file(path, std::any_cast<std::string>(new_map["title"]), std::any_cast<std::string>(new_map["author"]), std::to_string(post_id), logger_ptr);
                 else 
                     assist::create_mdx_from_template_file(path, std::any_cast<std::string>(new_map["title"]), std::to_string(post_id), logger_ptr);
-                return req->create_response().set_body(cp::serialize(res)).done();
+                return req->create_response().set_body(cp::serialize(page::get_page_content(post_id, pool_ptr))).done();
             }
             else {
                 logger_ptr->info( [endpoint]{return fmt::format("bad request from {}, not enough args", endpoint);});
@@ -131,15 +196,11 @@ namespace page::server {
                     logger_ptr->info( [endpoint]{return fmt::format("bad path from {}", endpoint);});
                     return req->create_response(restinio::status_forbidden()).done();
                 }
-                cp::query add_page("INSERT INTO \"posts\" (\"post_path\") VALUES ($1) RETURNING \"post_id\";");
 
-                auto tx = cp::tx(*pool_ptr, add_page);
-
-                pqxx::result res = add_page(path);
-                int post_id = res[0]["post_id"].as<int>();
+                int post_id = page::add_page_by_page(path, new_body["text"].GetString(), pool_ptr, logger_ptr);
                 logger_ptr->info( [endpoint, path]{return fmt::format("page added from {} to {}", endpoint, path);});
                 assist::create_file(path, new_body["text"].GetString(), logger_ptr);
-                return req->create_response().set_body(cp::serialize(res)).done();
+                return req->create_response().set_body(cp::serialize(page::get_page_content(post_id, pool_ptr))).done();
 
             }
             else return req->create_response(restinio::status_non_authoritative_information()).done();
@@ -159,14 +220,9 @@ namespace page::server {
             new_body.Parse(req->body().c_str());
 
             if (new_body.HasMember("post_id") && new_body.HasMember("likes")) {
-                cp::query update_likes("UPDATE \"posts\" SET \"likes\"=($1) WHERE \"post_id\"=($2);");
-
-                auto tx = cp::tx(*pool_ptr, update_likes);
-
-                pqxx::result res = update_likes(new_body["likes"].GetInt(), new_body["post_id"].GetInt());
-                tx.commit();
+                page::update_likes(new_body["post_id"].GetInt(), new_body["likes"].GetInt(), pool_ptr, logger_ptr);
                 logger_ptr->info( [endpoint, likes = new_body["likes"].GetInt()]{return fmt::format("likes updated to {} from {}", likes, endpoint);});
-                return req->create_response().done();
+                return req->create_response(restinio::status_ok()).done();
             }
             else return req->create_response(restinio::status_non_authoritative_information()).done();
         });
@@ -185,10 +241,7 @@ namespace page::server {
             if(!auth::is_authed(token, pool_ptr)) {
                 return req->create_response(restinio::status_unauthorized()).done();
             }
-            cp::query delete_post("DELETE FROM \"posts\" WHERE \"post_id\"=($1);");
-            auto tx = cp::tx(*pool_ptr, delete_post);
-            delete_post(post_id);
-            tx.commit();
+            page::delete_page(post_id,pool_ptr, logger_ptr);
             
             logger_ptr->info( [post_id]{return fmt::format("page deleted with id {}", post_id);});
             return req->create_response().set_body("ok").done();
@@ -208,11 +261,7 @@ namespace page::server {
 
             logger_ptr->info( [username]{return fmt::format("favourite posts request from {}", username);}); // delete
             
-            cp::query get_pages("SELECT username, post_id from \"favourite_posts\" WHERE \"username\"=($1);");
-
-            auto tx = cp::tx(*pool_ptr, get_pages);
-            
-            pqxx::result result = get_pages(username);
+            pqxx::result result = page::get_favourite_posts(username, pool_ptr);
             
             if(result.empty()) {
                 return req->create_response(restinio::status_ok()).done();
@@ -239,13 +288,8 @@ namespace page::server {
             logger_ptr->info( [username]{return fmt::format("add favourite post from {}", username);});
 
             if (new_body.HasMember("post_id")) {
-                cp::query add_page("INSERT INTO \"favourite_posts\" (\"post_id\", \"username\") VALUES ($1, $2) ON CONFLICT (\"post_id\", \"username\") DO NOTHING;");
                 try {
-                    auto tx = cp::tx(*pool_ptr, add_page);
-
-                    pqxx::result res = add_page(new_body["post_id"].GetInt(), username);
-    
-                    tx.commit();
+                    page::add_favourite_post(new_body["post_id"].GetInt(), username, pool_ptr, logger_ptr);
                 } catch (pqxx::unique_violation& e) {
                     logger_ptr->info( [endpoint]{return fmt::format("favourite post already exists from {}", endpoint);});
                     return req->create_response(restinio::status_forbidden()).done();
@@ -275,10 +319,7 @@ namespace page::server {
             }
             std::string username = auth::get_username(token, pool_ptr);
 
-            cp::query delete_post("DELETE FROM \"favourite_posts\" WHERE \"post_id\"=($1) AND \"username\"=($2);");
-            auto tx = cp::tx(*pool_ptr, delete_post);
-            delete_post(post_id, username);
-            tx.commit();
+            page::delete_favourite_post(post_id, username, pool_ptr, logger_ptr);
             
             logger_ptr->info( [post_id]{return fmt::format("favourite post deleted with id {}", post_id);});
             return req->create_response().set_body("ok").done();
--- page_server.h
+++ page_server.h
@@ -11,6 +11,28 @@
 #include "assist_funcs.h"
 #include "../basic/url_parser.h"
 #include "../basic/auth.h"
+
+namespace page {
+
+    pqxx::result get_page_content(int post_id, std::shared_ptr<cp::ConnectionsManager> pool_ptr);
+
+    pqxx::result get_page_author(std::string username, std::shared_ptr<cp::ConnectionsManager> pool_ptr);
+
+    int add_page_by_content(bool is_secret, bool is_published, int likes, std::string title, std::string author, std::string text, std::string post_path, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr);
+
+    int add_page_by_page(std::string post_path, std::string text, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr);
+
+    void update_likes(int post_id, int likes, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr);
+
+    void delete_page(int post_id, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr);
+
+    pqxx::result get_favourite_posts(std::string username, std::shared_ptr<cp::ConnectionsManager> pool_ptr);
+
+    pqxx::result add_favourite_post(int post_id, std::string username, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr);
+
+    void delete_favourite_post(int post_id, std::string username, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr);
+}
+
 namespace page::server {
     void get_page_content(std::unique_ptr<restinio::router::express_router_t<>>& router, std::shared_ptr<cp::ConnectionsManager> pool_ptr, std::shared_ptr<restinio::shared_ostream_logger_t> logger_ptr);
 
